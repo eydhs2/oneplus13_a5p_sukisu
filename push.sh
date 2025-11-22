73b533bd211a4d5e76cf234827a241c157f1a9fb
eydhs2 <552413609@qq.com>
2025-11-22 16:06:16 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,11 +1,4 @@
 git add .
 git commit -m "Futaba"
 git push
-diff --git a/fs/proc/task_mmu.c b/fs/proc/task_mmu.c
-@@ -1825,7 +1825,8 @@
--                vma = find_vma(mm, start_vaddr);
--                if (vma && vma->vm_file) {
--                        struct inode *inode = file_inode(vma->vm_file);
-+                struct vm_area_struct *vma = find_vma(mm, start_vaddr);
-+                if (vma && vma->vm_file) {
-+                        struct inode *inode = file_inode(vma->vm_file);
+
